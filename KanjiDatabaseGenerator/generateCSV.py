1486cf24bdad5f0ded300339a35ea55c6b33c8f0
Barbara Aguila Cudicio <aguila.cudicio@gmail.com>
2017-06-05 16:06:19 -0300

    complete dictionary + fix in csv generator

--- KanjiDatabaseGenerator/generateCSV.py
+++ KanjiDatabaseGenerator/generateCSV.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-  
 
 import csv 
+import re
 
 newfile = open('kanjidic.csv', 'w')
 f = open('kanjidic-utf8.txt', 'r')
-discard = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H",
-"I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+discard = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H", "I","J","K","L","M","N","O",
+"P","Q","R","S","T","U","V","W","X","Y","Z",
+"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+"r","s","t","u","v","w","x","y","z","{"]
 "In the file, S indicates stroke count, G grade and J JLPT level"
 for line in f:
 	line= line.rstrip()
 	readings = []
-	meanings = []
+	meanings = re.findall('\{.*?\}',line)
 	strokeCount = "0"
 	grade = "0"
 	jlpt = "0"
@@ -22,8 +25,6 @@ for line in f:
 			grade = element[1:]
 		elif element[0] == 'J':
 			jlpt = element[1:]
-		elif element[0] == '{':
-			meanings.append(element[1:-1])
 		elif not element[0] in discard:
 			readings.append(element)
 		reading = ' '.join(readings)
